708de5bd575e99554f5acdd6cbc78f8a6d5ba158
Juan Miguel Torres <wanmigs@gmail.com>
2022-11-02 14:44:41 +0800

    remove unused imports

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Header from "./components/layout/Header.vue";
 import Sidebar from "./components/layout/Sidebar.vue";
 </script>
